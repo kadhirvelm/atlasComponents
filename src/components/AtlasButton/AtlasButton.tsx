6f0fd4c3e5efe5652ff84b19a26fe08bac5f21fc
kadhirvelm <kadhirvelm@berkeley.edu>
2018-07-22 17:50:26 -0700

    Sass working, basic button integrated

--- src/components/AtlasButton/AtlasButton.tsx
+++ src/components/AtlasButton/AtlasButton.tsx
@@ -3,20 +3,45 @@ import * as React from "react";
 
 import "./AtlasButton.sass";
 
+export interface IAtlasButtonProps {
+  /**
+   * @param label Button label. Optional
+   */
+  label?: string;
+}
+
 /**
  * @class Simple button, extends HTML Button element
  */
 export class AtlasButton extends React.PureComponent<
-  React.HTMLProps<HTMLButtonElement>
+  React.HTMLProps<HTMLButtonElement> & IAtlasButtonProps
 > {
+  public constructor(
+    props: React.HTMLProps<HTMLButtonElement> & IAtlasButtonProps
+  ) {
+    super(props);
+    if (this.props.label === undefined && this.props.children === undefined) {
+      throw new Error(
+        "Cannot leave both label and children blank. Must specify at least one."
+      );
+    }
+  }
+
   public render() {
     return (
       <button
         {...this.props}
         className={classNames("atlas-button", this.props.className)}
       >
-        <div className="centered"> {this.props.children} </div>
+        {this.renderContents()}
       </button>
     );
+  }
+
+  private renderContents() {
+    if (this.props.label !== undefined) {
+      return <div className="atlas-label">{this.props.label}</div>;
+    }
+    return this.props.children;
   }
 }
