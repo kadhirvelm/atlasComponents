1ad862f8a025c65cd5c3c4ede5890c325b046f2f
kadhirvelm <kadhirvelm@berkeley.edu>
2018-07-26 21:34:36 -0700

    Basic button added

---
+++ src/components/AtlasButton/AtlasButton.tsx
@@ -0,0 +1,47 @@
+import classNames from "classnames";
+import * as React from "react";
+
+import "./AtlasButton.sass";
+
+export interface IAtlasButtonProps {
+  /**
+   * @param label Button label. Optional
+   */
+  label?: string;
+}
+
+/**
+ * @class Simple button, extends HTML Button element
+ */
+export class AtlasButton extends React.PureComponent<
+  React.HTMLProps<HTMLButtonElement> & IAtlasButtonProps
+> {
+  public constructor(
+    props: React.HTMLProps<HTMLButtonElement> & IAtlasButtonProps
+  ) {
+    super(props);
+    if (this.props.label === undefined && this.props.children === undefined) {
+      throw new Error(
+        "Cannot leave both label and children blank. Must specify at least one."
+      );
+    }
+  }
+
+  public render() {
+    return (
+      <button
+        {...this.props}
+        className={classNames("atlas-button", this.props.className)}
+      >
+        {this.renderContents()}
+      </button>
+    );
+  }
+
+  private renderContents() {
+    if (this.props.label !== undefined) {
+      return <div className="atlas-label">{this.props.label}</div>;
+    }
+    return this.props.children;
+  }
+}
--- src/components/tests/button.test.tsx
+++
@@ -1,7 +0,0 @@
-import { TestClass } from "../button";
-
-it("contains the test function", () => {
-  const testClass = new TestClass();
-  expect(testClass).not.toBeUndefined();
-  expect(testClass.testFunction()).toEqual("Reaching test function");
-});
