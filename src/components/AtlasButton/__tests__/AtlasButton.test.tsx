26ddc420d086942bae64948e94147dae6fd45635
kadhirvelm <kadhirvelm@berkeley.edu>
2018-07-26 21:29:38 -0700

    Enzyme adpater added, and removed doc tests

--- src/components/AtlasButton/__tests__/AtlasButton.test.tsx
+++ src/components/AtlasButton/__tests__/AtlasButton.test.tsx
@@ -5,7 +5,18 @@ import { AtlasButton } from "..";
 
 describe("AtlasButton tests", () => {
   it("renders without crashing", () => {
-    const testButton = shallow(<AtlasButton />);
-    expect(testButton).not.toBeUndefined();
+    const testButton = shallow(<AtlasButton>Test</AtlasButton>);
+    expect(testButton).toBeDefined();
+  });
+
+  it("render with a label", () => {
+    const testButton = shallow(<AtlasButton label="Test" />);
+    expect(testButton).toBeDefined();
+  });
+
+  it("throws an error when neither label nor children are specified", () => {
+    expect(() => {
+      shallow(<AtlasButton />);
+    }).toThrowError();
   });
 });
